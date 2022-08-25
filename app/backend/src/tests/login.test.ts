b441e1c6c57f058740927ed9e033d569daf56fb9
Vinicius Gomes <viniplay0599@gmail.com>
2022-08-25 16:13:26 -0300

    test: implements login tests

--- app/backend/src/tests/login.test.ts
+++ app/backend/src/tests/login.test.ts
@@ -1,5 +1,6 @@
 import * as sinon from 'sinon';
 import * as chai from 'chai';
+import { hash } from 'bcryptjs';
 
 //@ts-ignore
 
@@ -18,24 +19,65 @@ const loginMock: IUser = {
   id: 1,
   username: 'any-name',
   role: 'any-role',
-  email: 'any-email',
-  password: 'any-string',
+  email: 'email@email.com',
+  password: '$2b$10$//DXiVVE59p7G5k/4Klx/ezF7BI42QZKmoOD0NDvUuqxRE5bFFBLy',
 }
+
+const expectedBody = {
+  email: 'email@email.com',
+  password: '123456gjfnsn'
+};
+
+const TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwicm9sZSI6ImFkbWluIiwiaWF0IjoxNjU0NTI3MTg5fQ.XS_9AA82iNoiVaASi0NtJpqOQ_gHSHhxrpIdigiT-fc'
 
 describe('Testing login route:', function() {
   let chaiHttpResponse: Response;
 
-  before(async () => {
-    sinon.stub(Users, 'findOne').resolves(loginMock as Users);
-  })
-
-  after(async () => {
-    (Users.findOne as sinon.SinonStub).restore();
-  })
-
   it('Should return a token:', async function() {
-    chaiHttpResponse = await chai.request(app).post('/login');
+    const passwordHash = await hash('123456gjfnsn', 8);
+
+    sinon.stub(Users, 'findOne').resolves({
+      id: 1,
+      username: 'any-name',
+      role: 'any-role',
+      email: 'email@email.com',
+      password: passwordHash,
+    } as Users);
+
+    chaiHttpResponse = await chai.request(app).post('/login').send(expectedBody);
 
     expect(chaiHttpResponse.body).to.have.key('token').and.not.to.be.an('undefined');
+  });
+
+  it('Should return status 400 if email is not informed:', async function() {
+    chaiHttpResponse = await chai.request(app).post('/login').send(expectedBody.password);
+
+    expect(chaiHttpResponse).to.have.status(400);
+
+    expect(chaiHttpResponse.body).to.be.deep.equal({ message: 'All fields must be filled' });
+  });
+
+  it('Should return status 400 if password is not informed', async function() {
+    chaiHttpResponse = await chai.request(app).post('/login').send(expectedBody.email);
+
+    expect(chaiHttpResponse).to.have.status(400);
+
+    expect(chaiHttpResponse.body).to.be.deep.equal({ message: 'All fields must be filled' });
+  });
+  
+  it('Should return status 401 in case of invalid email', async function() {
+    chaiHttpResponse = await chai.request(app).post('/login').send({ email: 'any@email', password: 'any-password' });
+
+    expect(chaiHttpResponse).to.have.status(401);
+
+    expect(chaiHttpResponse.body).to.be.deep.equal({ message: 'Incorrect email or password' });
+  })
+
+  it('Should return status 401 in case of invalid password:', async function() {
+    chaiHttpResponse = await chai.request(app).post('/login').send({ email: 'any@email.com', password: 'teste' });
+
+    expect(chaiHttpResponse).to.have.status(401);
+
+    expect(chaiHttpResponse.body).to.be.deep.equal({ message: 'Incorrect email or password' });
   })
 })
